be9f91731c0eb15f2026904b7ded9e04cb6aeead
Даша <golovinad84@gmail.com>
2025-03-13 00:53:55 +0500

    Добавила логи в UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionNotMetException;
@@ -14,6 +15,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final Map<Long, User> users = new HashMap<>();
 
     @GetMapping
@@ -23,38 +25,45 @@ public class UserController {
 
     @PostMapping
     public User postUser(@Valid @RequestBody User newUser) {
-        if (newUser.getLogin().contains(" ")) {
-            throw new ConditionNotMetException("Логин пользователя не может содержать пробелы");
-        }
-        if (newUser.getName() == null) {
-            newUser.setName(newUser.getLogin());
-        }
+        log.debug("Запрос на добавление пользователя {}", newUser.toString());
+        validator(newUser);
         newUser.setId(getNextId());
         users.put(newUser.getId(), newUser);
+        log.debug("Пользователь зарегестрировался {}", newUser.toString());
         return newUser;
     }
 
     @PutMapping
     public User putUser(@Valid @RequestBody User newUser) {
+        log.debug("Запрос на изменение данных о пользователе {}", newUser.toString());
         if (newUser.getId() == null) {
-            throw new ConditionNotMetException("В запосе на обновление не указан id");
+            log.trace("В запросе не бы указан id пользователя");
+            throw new ConditionNotMetException("В запросе на обновление не указан id");
         }
 
         if (users.containsKey(newUser.getId())) {
+            validator(newUser);
             User oldUser = users.get(newUser.getId());
-            if (newUser.getLogin().contains(" ")) {
-                throw new ConditionNotMetException("Логин пользователя не может содержать пробелы");
-            }
-            if (newUser.getName() == null) {
-                newUser.setName(newUser.getLogin());
-            }
+            log.trace("Пользователь чьи данные будем обнавлять {}", oldUser);
             oldUser.setName(newUser.getName());
             oldUser.setLogin(newUser.getLogin());
             oldUser.setEmail(newUser.getEmail());
             oldUser.setBirthday(newUser.getBirthday());
             return oldUser;
         }
+        log.debug("Пользователь с id {} не был найден. Не удалось осуществить запрос: {}", newUser.getId(), newUser.toString());
         throw new NotFoundException("Пользователь с id " + newUser.getId() + " не был найден");
+    }
+
+    private void validator(User newUser) {
+        if (newUser.getLogin().contains(" ")) {
+            log.trace("Неправильный логин пользователя {}", newUser.getLogin());
+            throw new ConditionNotMetException("Логин пользователя не может содержать пробелы");
+        }
+        if (newUser.getName() == null) {
+            log.trace("В запросе не было имени пользователя, поэтому запишем логин");
+            newUser.setName(newUser.getLogin());
+        }
     }
 
     private Long getNextId() {
