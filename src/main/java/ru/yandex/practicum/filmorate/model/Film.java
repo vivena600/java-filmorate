b92e0f30feee6b6acf652dd266e277f527bb18c6
Даша <golovinad84@gmail.com>
2025-03-13 16:34:02 +0500

    Добавила тесты для Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
+@Builder(toBuilder = true)
 @Data
 public class Film {
     private final static int MAXSIZEDESCRIPTION = 200;
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTests.java
@@ -0,0 +1,82 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import jakarta.validation.Validator;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmTests {
+    private FilmController controller;
+    private Validator validator;
+    private static Film film;
+
+    @BeforeEach
+    public void startTest() {
+        film = Film.builder().
+                name("film name")
+                .duration(3)
+                .description("description film's")
+                .releaseDate(LocalDate.parse("2025-03-25"))
+                .build();
+        System.out.println(film.getDuration());
+        controller = new FilmController();
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void getFilms() {
+
+    }
+
+    @Test
+    void createFilm() {
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+        assertTrue(violations.isEmpty(), "Программа увидела ошибку, которой не должно быть");
+    }
+
+    @Test
+    void createFilmWithFailDuration() {
+        Film newfilm = film.toBuilder()
+                .duration(-3)
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(newfilm);
+        assertFalse(violations.isEmpty(), "Программа не увидела ошибку, связанную с отрицательной " +
+                "продолжительностью фильма");
+    }
+
+    @Test
+    void createFilmWithFailDescription() {
+        Film newfilm = film.toBuilder()
+                .description("a".repeat(201))
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(newfilm);
+        assertFalse(violations.isEmpty(), "Программа не увидела ошибку, связанную с некорректным " +
+                "описанием фильма");
+
+        Film newfilm2 = film.toBuilder()
+                .description("")
+                .build();
+        Set<ConstraintViolation<Film>> violations2 = validator.validate(newfilm2);
+        assertFalse(violations2.isEmpty(), "Программа не увидела ошибку, связанную с записью пустого " +
+                "описания фильма");
+    }
+
+    @Test
+    void createFilmWithFailName() {
+        Film newfilm = film.toBuilder()
+                .name("")
+                .build();
+        Set<ConstraintViolation<Film>> violations = validator.validate(newfilm);
+        assertFalse(violations.isEmpty(), "Программа не увидела ошибку, связанную с пустым " +
+                "названием фильма");
+    }
+}
